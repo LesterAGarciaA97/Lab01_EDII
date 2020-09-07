be14418dd93ba63225e64118995bee36ae7cd0ef
BrayanLeal <46784353+BrayanLeal@users.noreply.github.com>
2020-09-06 23:12:53 -0600

    Se agrego comparador de objetos

---
+++ Solución/Lab01_EDII/Lab01_EDII/Models/Comparador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01_EDII.Models
+{
+    public class Comparador
+    {
+        public int CompararPorNombre(Movie Movie1, Movie Movie2)
+        {
+            return Movie1 == null || Movie2 == null ? 1 : Movie1.Title.CompareTo(Movie2.Title);
+        }
+    }
+}
