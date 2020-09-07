947aaaf7fb5dde81b55afcb9b06cdf5747e6e660
BrayanLeal <46784353+BrayanLeal@users.noreply.github.com>
2020-09-06 23:03:14 -0600

    Se agrego patron de diseno singelton

---
+++ Solución/Lab01_EDII/Lab01_EDII/Models/DataSingelton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01_EDII.Models
+{
+    public class DataSingelton
+    {
+        private static DataSingelton _instance = null;
+        public static DataSingelton Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new DataSingelton();
+                return _instance;
+            }
+        }
+        private static Comparador _Comparador = new Comparador();
+        public ArbolMulticamino<Movie> Arbolb = new ArbolMulticamino<Movie>(5, _Comparador.CompararPorNombre);
+    }
+}
